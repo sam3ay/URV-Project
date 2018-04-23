746d49109dba8fb48483e3d89708504d868eec8a
Sam Ay <30203976+sam3ay@users.noreply.github.com>
2018-04-22 19:30:38 -0700

    Update (#7)
    * modularize (#2)
    
    * clean up
    
    * os pathing (#5)
    
    * modularize (#2) (#3) (#6)

--- scripts/build_meta.py
+++ scripts/build_meta.py
@@ -1,6 +1,6 @@
-from dict_extract import *
-from dictquery import *
-from xmldictconv import *
+import dict_extract
+import dictquery
+import xmldictconv
 import os
 
 
@@ -8,9 +8,11 @@ def build_metadata(identifier, path, list_of_paths, input_dict={}):
     """Generates metadata from xml file
 
     Args:
-        identifier: string, Name of the dictionary information will be extracted from
+        identifier: string, Name of the dictionary from which information
+                    will be extracted
         path: string, location of xml file
-        input_dict: optional variable, dictionary, contains metadata information
+        input_dict: optional variable, dictionary,
+                    contains metadata information
     Returns:
         list of strings, metadata information
     Notes:
@@ -19,13 +21,16 @@ def build_metadata(identifier, path, list_of_paths, input_dict={}):
         'PLATFORM/ILLUMINA/INSTRUMENTAL_MODEL', ILLUMINA, SC
     """
     # retrieve parent directory from path and specify experiment xml file
-    exper_dict = input_dict
     exp_loc = os.path.abspath(os.path.join(path, os.pardir))
     exp_id = os.path.split(exp_loc)[1]
     exp_name = exp_loc + os.sep + exp_id + '.experiment.xml'
     try:
-        experiment = next(gen_dict_extract(identifier, exper_dict[exp_name]), None)
+        experiment = next(
+                dict_extract.gen_dict_extract(
+                    identifier, input_dict[exp_name]), None)
     except KeyError:
-        exper_dict[exp_name] = xmldictconv(exp_name)
-        experiment = next(gen_dict_extract(identifier, exper_dict[exp_name]), None)
-    return (exper_dict, dictquery(experiment, list_of_paths))
+        input_dict[exp_name] = xmldictconv.xmldictconv(exp_name)
+        experiment = next(
+                dict_extract.gen_dict_extract(
+                    identifier, input_dict[exp_name]), None)
+    return (input_dict, dictquery.dictquery(experiment, list_of_paths))
