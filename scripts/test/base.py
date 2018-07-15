faca0e733b6c89b620a0588643b629940172e4ce
Sam Ay <30203976+sam3ay@users.noreply.github.com>
2018-07-15 22:53:27 +0000

    gcs set up

--- scripts/test/base.py
+++ scripts/test/base.py
@@ -1,25 +1,19 @@
 import unittest
-from google.cloud import storage
+import gstorage
 
 
 class TestUrvMethods(unittest.TestCase):
-    """
+    """Establish testing environment
     """
 
     def setUp(self):
         """
-        Creates temporary directory
         Provides gcs account credentials to all tests
+        Retrieves google bucket
+        WIP: Creates temporary directory
         """
-        storage_client = storage.Client.from_service_account_json(
-                'project_service_account.json')
-
-    def tearDown(self):
-        """
-        """
-    def bucketexist(self):
-        """
-        """
+        storage_client = gstorage.google_auth('authenticate key')
+        storage_bucket = gstorage.get_bucket(storage_client, 'bucket name')
 
 
 if __name__ == '__main__':
