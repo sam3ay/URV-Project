72272b0cd1ffc5d104f93bb201b4b30df24cf262
Sam Ay <30203976+sam3ay@users.noreply.github.com>
2018-08-14 23:32:12 +0000

    Dict Query Exception handing

--- tsvconv/dictquery.py
+++ tsvconv/dictquery.py
@@ -8,8 +8,10 @@ def dictquery(input_dict, listofpath):
         listofpath (list): path of desired value
     Returns:
         list of values at path specified by input list
+    Raises:
+        TypeError: When key is not in dictionary
+        AttributeError: When reduce runs dict.get on non dictionary object
     Notes:
-        KeyError hidden by nonetype
     """
     output_list = []
     try:
@@ -17,5 +19,7 @@ def dictquery(input_dict, listofpath):
             output_list.append(
                     reduce(dict.get, path.split('/'), input_dict))
     except TypeError:
+        output_list.append(None)
+    except AttributeError:
         output_list.append(None)
     return output_list
