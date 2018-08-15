121b1993192229ffbb8803077cf372863eb1a3c2
Sam Ay <30203976+sam3ay@users.noreply.github.com>
2018-08-15 01:51:43 +0000

    setup

--- tsvconv/fastqtsvfile.py
+++ tsvconv/fastqtsvfile.py
@@ -55,6 +55,26 @@ def tsvbuild(gcs, pattern, list_of_paths, tsv_name):
                 break
     return tsv_name
 
+def tsvbuild(json_path, gcs_bucket, pattern, list_of_paths, tsv_name):
+    """builds a tsv file from a directory of paired files
+
+    Retrieves location of pairs of files matching pattern and retrieves
+    metadata information from the parent directory located in an xml file
+    Assumes paired files differences occur after an underscore '_'.
+
+    Args:
+        json_path (str): path to service account json
+        gcs (str): google cloud bucket name, recursively searched
+        pattern (str): file identifying pattern being searched
+        tsv_name (str): filename or tsv file
+        list_of_paths (list): containing location of desired values
+
+     Returns:
+        str: location of tsv file with format preceded by list_of_paths followed by files
+           in nested dictionary
+    """
+
+
 
 if __name__ == '__main__':
     darg = argproc.parse_args()
---
+++ tsvconv/tsvbuildtest.py
@@ -0,0 +1,8 @@
+from tsvconv import tsvbuild
+from tsvconv.test import base
+
+class TestTsvBuild(base.TestUrvMethods):
+    
+    @classmethod
+    def setUpClass(cls):
+        
