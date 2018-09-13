f2c8e23b563ac23970f320cb0ac3172d3dc95609
Sam Ay <30203976+sam3ay@users.noreply.github.com>
2018-09-13 17:16:41 +0000

    working state

--- tsvconv/dictquery.py
+++ tsvconv/dictquery.py
@@ -1,29 +1,4 @@
-from functools import reduce
 import asyncio
-
-
-def dictquery(input_dict, listofpath):
-    """Fetches data from nested dictionary
-    Args:
-        input_dict (dict): Dictionary with desired values
-        listofpath (list): path of desired value
-    Returns:
-        list of values at path specified by input list
-    Raises:
-        TypeError: When key is not in dictionary
-        AttributeError: When reduce runs dict.get on non dictionary object
-    Notes:
-    """
-    output_list = []
-    try:
-        for path in listofpath:
-            output_list.append(
-                    reduce(dict.get, path.split('/'), input_dict))
-    except TypeError:
-        output_list.append(None)
-    except AttributeError:
-        output_list.append(None)
-    return output_list
 
 
 async def dict_endpoints(input_dict, endpoint_dict, key=None):
--- tsvconv/test/tsvbuildtest.py
+++ tsvconv/test/tsvbuildtest.py
@@ -5,34 +5,6 @@ import magic
 
 class TestTsvBuild(base.TestUrvMethods):
 
-    def testdictbuild(self):
-        keys = ['foo', 'bar', 'hello', 'why', 'is', 'there']
-        values = [0, 1, 2, 3, 4, 5]
-        wrong_size_list = [0, 1, 3]
-        pass_dict = tsvbuild.dictbuild(keys, values)
-        key_check = 'foo'
-        exp_val = 2
-        self.assertIsInstance(
-                pass_dict,
-                dict,
-                msg='Is not a dictionary'
-                )
-        self.assertTrue(
-                key_check in pass_dict,
-                msg="Key Error: key doesn't exist in dictionary")
-        self.assertEqual(
-                pass_dict['hello'],
-                exp_val,
-                msg="Unexpected value returned")
-        self.assertEqual(
-                pass_dict.keys(),
-                keys,
-                msg="Unexpected key order")
-        with self.assertRaises(
-                ValueError,
-                msg="Incorrect list size accepted"):
-            tsvbuild.dictbuild(keys, wrong_size_list)
-
     def testtsvwriter(self):
         input_dict = {'foo': 'where', 'bar': 'there'}
         file_test = '/root/URV-Project/tsv_test.tsv'
