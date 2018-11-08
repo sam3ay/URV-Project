258a72948cb5470e0620ecb7ffb634c77444b89d
Sam Ay <30203976+sam3ay@users.noreply.github.com>
2018-11-08 23:26:42 +0000

    role exclusion

--- scripts/gatk-dataproc-init.sh
+++ scripts/gatk-dataproc-init.sh
@@ -8,11 +8,11 @@ readonly PROJECT="$(/usr/share/google/get_metadata_value ../project/project-id)"
 SERVICE_ACCOUNT=$(/usr/share/google/get_metadata_value attributes/service_account)
 JSON_LOCATION=$(/usr/share/google/get_metadata_value attributes/json_location)
 
-
-mkdir /app
-gsutil cp $SERVICE_ACCOUNT $JSON_LOCATION
-# Env variables necessary for GCS hadoop connector to be configured properly
-echo 'export HELLBENDER_TEST_PROJECT=$PROJECT' | tee -a /etc/profile
-echo 'export HELLBENDER_JSON_SERVICE_ACCOUNT_KEY=$JSON_LOCATION' | tee -a /etc/profile
-
-source /etc/profile
+if [[ "${ROLE}" == 'Master' ]]; then
+    mkdir /app
+    gsutil cp $SERVICE_ACCOUNT $JSON_LOCATION
+    # Env variables necessary for GCS hadoop connector to be configured properly
+    echo 'export HELLBENDER_TEST_PROJECT=$PROJECT' | tee -a /etc/profile
+    echo 'export HELLBENDER_JSON_SERVICE_ACCOUNT_KEY=$JSON_LOCATION' | tee -a /etc/profile
+    source /etc/profile
+fi
